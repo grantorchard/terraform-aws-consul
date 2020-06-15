a1572c63181602d63e7a9b05028b21518233df5f
Grant Orchard <go@hashicorp.com>
2020-06-15 11:45:33 +1000

    initial commit

--- variables.tf
+++ variables.tf
@@ -1,1 +1,38 @@
 
+variable hostname {
+  type = string
+  default = "consul"
+}
+
+variable key_name {
+  type    = string
+  default = "go"
+}
+
+variable tags {
+  type = map
+  default = {
+    TTL   = "48"
+    owner = "Grant Orchard"
+  }
+}
+
+variable instance_type {
+  type    = string
+  default = "t2.medium"
+}
+
+variable slack_webhook {
+  type = string
+  default = ""
+}
+
+variable private_ip {
+  type = string
+  default = "10.0.101.162"
+}
+
+variable consul_datacenter {
+  type = string
+  default = "AWS"
+}
